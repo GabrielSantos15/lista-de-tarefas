89bda2e2610d7b229a74513f95c4aefa054a5e23
Gabriel dos Santos <gs303291@gmail.com>
2023-11-11 17:33:50 -0300

    botão para concluir tarefa

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const inputText = document.querySelector("#inputText");
 const sectionLista = document.querySelector("#sectionLista ul");
-const outLista =  document.querySelector("#sectionLista h2> output");
+const outLista = document.querySelector("#sectionLista h2> output");
 
 const lista = [];
 
@@ -8,22 +8,20 @@ function atualisar() {
   sectionLista.innerHTML = ``;
 
   lista.forEach((item, posicao) => {
-
     sectionLista.innerHTML += `
-    <li>
-        <input type="checkbox">
+    <li class="${item.concluida && 'done'}">
+        <input type="checkbox" oninput="concluir(${posicao})" ${item.concluida && 'checked'}>
         <p>${lista[posicao].tarefa}</p>
         <span class="material-symbols-outlined" onclick="apagar(${posicao})">close</span>
     </li>
     `;
   });
 
-  outLista.innerHTML = lista.length
+  outLista.innerHTML = lista.length;
 }
 
 function adicionar() {
-
-
+  if(inputText.value == '')return
   lista.unshift({
     tarefa: inputText.value,
     concluida: false,
@@ -31,11 +29,16 @@ function adicionar() {
 
   inputText.value = "";
 
-  atualisar()
+  atualisar();
 }
-function apagar(posicao){
-    console.log(posicao)
 
-    lista.splice(posicao,1);
-    atualisar();
+function concluir(posicao){
+  lista[posicao].concluida = !lista[posicao].concluida
+  atualisar();
+}
+function apagar(posicao) {
+  console.log(posicao);
+
+  lista.splice(posicao, 1);
+  atualisar();
 }
