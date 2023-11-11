43206b34096dc6b747bd0d5772fd46376516c053
Gabriel dos Santos <gs303291@gmail.com>
2023-11-11 18:06:08 -0300

    Guardando lista no navegador

--- js/index.js
+++ js/index.js
@@ -2,15 +2,28 @@ const inputText = document.querySelector("#inputText");
 const sectionLista = document.querySelector("#sectionLista ul");
 const outLista = document.querySelector("#sectionLista h2> output");
 
-const lista = [];
+let lista = [];
+
+function recuperarLista() {
+  const storageLista = localStorage.getItem("lista");
+
+  if (storageLista) {
+    lista = JSON.parse(storageLista);
+  }
+
+  atualisar();
+}
+recuperarLista();
 
 function atualisar() {
   sectionLista.innerHTML = ``;
 
   lista.forEach((item, posicao) => {
     sectionLista.innerHTML += `
-    <li class="${item.concluida && 'done'}">
-        <input type="checkbox" oninput="concluir(${posicao})" ${item.concluida && 'checked'}>
+    <li class="${item.concluida && "done"}">
+        <input type="checkbox" oninput="concluir(${posicao})" ${
+      item.concluida && "checked"
+    }>
         <p>${lista[posicao].tarefa}</p>
         <span class="material-symbols-outlined" onclick="apagar(${posicao})">close</span>
     </li>
@@ -18,10 +31,12 @@ function atualisar() {
   });
 
   outLista.innerHTML = lista.length;
+
+  localStorage.setItem("lista", JSON.stringify(lista));
 }
 
 function adicionar() {
-  if(inputText.value == '')return
+  if (inputText.value == "") return;
   lista.unshift({
     tarefa: inputText.value,
     concluida: false,
@@ -32,8 +47,8 @@ function adicionar() {
   atualisar();
 }
 
-function concluir(posicao){
-  lista[posicao].concluida = !lista[posicao].concluida
+function concluir(posicao) {
+  lista[posicao].concluida = !lista[posicao].concluida;
   atualisar();
 }
 function apagar(posicao) {
