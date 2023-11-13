c0487b9b88604a284622036beae2a081df103285
Gabriel dos Santos <gs303291@gmail.com>
2023-11-13 20:23:22 -0300

    Adicionando botão de edição

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
+const formLista = document.querySelector("#form");
 const inputText = document.querySelector("#inputText");
 const sectionLista = document.querySelector("#sectionLista ul");
 const outLista = document.querySelector("#sectionLista h2> output");
 
+const formEdit = document.querySelector("#formEdit");
+const inputEdit = document.querySelector("#inputEdit");
+
 let lista = [];
+let editElemnt;
 
 function recuperarLista() {
   const storageLista = localStorage.getItem("lista");
@@ -16,6 +21,9 @@ function recuperarLista() {
 recuperarLista();
 
 function atualisar() {
+  formLista.style.display = "flex";
+  formEdit.style.display = "none";
+
   sectionLista.innerHTML = ``;
 
   lista.forEach((item, posicao) => {
@@ -25,7 +33,10 @@ function atualisar() {
       item.concluida && "checked"
     }>
         <p>${lista[posicao].tarefa}</p>
-        <span class="material-symbols-outlined" onclick="apagar(${posicao})">close</span>
+        <div>
+          <span class="material-symbols-outlined" onclick="editar(${posicao})">edit</span>
+          <span class="material-symbols-outlined" onclick="apagar(${posicao})">close</span>
+        <div>
     </li>
     `;
   });
@@ -36,7 +47,15 @@ function atualisar() {
 }
 
 function adicionar() {
-  if (inputText.value == "") return;
+  if (inputText.value == "") {
+    const form = document.querySelector("#form");
+
+    form.style.animation = "";
+    setTimeout(() => {
+      form.style.animation = "shake .5s linear";
+    }, 5);
+    return;
+  }
   lista.unshift({
     tarefa: inputText.value,
     concluida: false,
@@ -57,3 +76,28 @@ function apagar(posicao) {
   lista.splice(posicao, 1);
   atualisar();
 }
+
+function clearAll() {
+  const confirmacao = window.confirm("Deseja apagar sua lista?");
+
+  if (confirmacao) {
+    lista.splice(0, lista.length);
+    atualisar();
+  }
+}
+
+function editar(posicao) {
+  formEdit.style.display = "flex";
+  formLista.style.display = "none";
+
+  inputEdit.value = lista[posicao].tarefa;
+  editElemnt = posicao;
+}
+function alterar() {
+  lista[editElemnt].tarefa = inputEdit.value;
+  atualisar();
+}
+
+inputText.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") adicionar();
+});
