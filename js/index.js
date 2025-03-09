08503941adc2f62248484d6d830299b4f28f41c4
Gabriel <gs303291@gmail.com>
2025-03-09 16:37:46 -0300

    melhorando o design

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
-const formLista = document.querySelector("#form");
+const formLista = document.querySelector("#formLista");
 const inputText = document.querySelector("#inputText");
+const dateInput = document.querySelector("#dateInput");
+const timeInput = document.querySelector("#timeInput");
+const nivelInput = document.querySelector("#nivelInput");
+
 const sectionLista = document.querySelector("#sectionLista ul");
 const outLista = document.querySelector("#sectionLista h2> output");
 
 const formEdit = document.querySelector("#formEdit");
 const inputEdit = document.querySelector("#inputEdit");
+
+resetInputs();
 
 let lista = [];
 let editElemnt;
@@ -16,65 +22,87 @@ function recuperarLista() {
     lista = JSON.parse(storageLista);
   }
 
-  atualisar();
+  atualizar();
 }
 recuperarLista();
 
-function atualisar() {
+function atualizar() {
   formLista.style.display = "flex";
   formEdit.style.display = "none";
 
   sectionLista.innerHTML = ``;
 
-  lista.forEach((item, posicao) => {
-    sectionLista.innerHTML += `
-    <li class="${item.concluida && "done"}">
-        <input type="checkbox" oninput="concluir(${posicao})" ${
-      item.concluida && "checked"
-    }>
-        <p>${lista[posicao].tarefa}</p>
-        <div>
-          <span class="material-symbols-outlined" onclick="editar(${posicao})">edit</span>
-          <span class="material-symbols-outlined" onclick="apagar(${posicao})">close</span>
-        <div>
-    </li>
+  if (lista.length === 0) {
+    sectionLista.innerHTML = `<li>Nenhuma tarefa encontrada</li>`;
+  } else {
+    lista.forEach((item, posicao) => {
+      sectionLista.innerHTML += `
+      <li class="${item.concluida && "done"} ${
+        Validade(lista[posicao].fim.data, lista[posicao].fim.hora) &&
+        !item.concluida &&
+        "vencida"
+      }">
+        <div class="endTime">
+          <p>${formatarData(lista[posicao].fim.data)} ${
+        lista[posicao].fim.hora
+      }</p>
+          <div>${lista[posicao].nivel}</div>
+        </div>
+        <div class="tarefa">
+          <input type="checkbox" oninput="concluir(${posicao})" ${
+        item.concluida && "checked"
+      }>
+          <p>${lista[posicao].tarefa}</p>
+          <div>
+            <span class="material-symbols-outlined" onclick="editar(${posicao})">edit</span>
+            <span class="material-symbols-outlined" onclick="apagar(${posicao})">close</span>
+          <div>
+        </div>
+      </li>
     `;
-  });
+    });
+  }
 
   outLista.innerHTML = lista.length;
 
   localStorage.setItem("lista", JSON.stringify(lista));
 }
 
-function adicionar() {
-  if (inputText.value == "") {
-    const form = document.querySelector("#form");
+formLista.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  // verifica se o input esta vazio e aplica o efeito shake
+  if (inputText.value == "") {
+    const form = document.querySelector(".taskInputContainer");
     form.style.animation = "";
     setTimeout(() => {
       form.style.animation = "shake .5s linear";
     }, 5);
     return;
   }
+
+  // adiciona a tarefa no inicio da lista
   lista.unshift({
     tarefa: inputText.value,
+    fim: { data: dateInput.value, hora: timeInput.value },
+    nivel: nivelInput.value,
     concluida: false,
   });
 
-  inputText.value = "";
+  // esvazia os inputs
+  resetInputs();
 
-  atualisar();
-}
+  atualizar();
+});
 
 function concluir(posicao) {
   lista[posicao].concluida = !lista[posicao].concluida;
-  atualisar();
+  atualizar();
 }
-function apagar(posicao) {
-  console.log(posicao);
 
+function apagar(posicao) {
   lista.splice(posicao, 1);
-  atualisar();
+  atualizar();
 }
 
 function clearAll() {
@@ -82,7 +110,7 @@ function clearAll() {
 
   if (confirmacao) {
     lista.splice(0, lista.length);
-    atualisar();
+    atualizar();
   }
 }
 
@@ -93,11 +121,29 @@ function editar(posicao) {
   inputEdit.value = lista[posicao].tarefa;
   editElemnt = posicao;
 }
-function alterar() {
+
+formEdit.addEventListener("submit", (event) => {
+  event.preventDefault();
   lista[editElemnt].tarefa = inputEdit.value;
-  atualisar();
+  atualizar();
+});
+
+function formatarData(data) {
+  const [ano, mes, dia] = data.split("-");
+  return `${dia}/${mes}/${ano}`;
+}
+function resetInputs() {
+  const now = new Date();
+  now.setHours(now.getHours() + 1); // Adiciona uma hora à data/hora atuais
+  
+  inputText.value = "";
+  dateInput.value = now.toISOString().split("T")[0];
+  timeInput.value = now.toTimeString().slice(0, 5);
 }
 
-inputText.addEventListener("keydown", (event) => {
-  if (event.key == "Enter") adicionar();
-});
+function Validade(data, hora) {
+  const agora = new Date();
+  const dataHoraTarefa = new Date(`${data}T${hora}`);
+
+  return dataHoraTarefa < agora;
+}
